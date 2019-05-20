a940408283aa5930da7186c312debef1f90953c9
Alejandra Diaz <ale.almeidaluna@gmail.com>
2019-05-20 15:48:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if "#{phrase}" == "#{phrase}".upcase
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif "#{phrase}" != "#{phrase}".upcase
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   else "#{phrase}" == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
